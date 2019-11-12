497d098fa12441e3284d0dfc96244d412c36e1f3
Paweł Zmarzły <pawo2500@gmail.com>
2019-11-12 16:40:50 +0100

    battery state handler

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import subprocess
 import argparse
 from xml.dom import minidom
 
+def tryRunning(argv):
+    try:
+        subprocess.Popen(argv)
+    except Exception as e:
+        print("Error: %s" % str(e))
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--hci", metavar="INT", type=int, default=0,
     help="use Bluetooth hciX (default 0, see `hcitool dev')")
@@ -26,11 +32,12 @@ signal.signal(signal.SIGINT, signal_handler)
 notificationSource = None
 controlPoint = None
 dataSource = None
+iphoneID = None
 
-ancsId = "7905f431-b5ce-4e99-a40f-4b1e122d00d0"
-notificationSourceId = "9fbf120d-6301-42d9-8c58-25e699a21dbd"
-controlPointId = "69d1d8f3-45e1-49a8-9821-9bbdfdaad9d9"
-dataSourceId = "22eac6e9-24d6-4bb5-be44-b36ace7c7bfb"
+ancsID = "7905f431-b5ce-4e99-a40f-4b1e122d00d0"
+notificationSourceID = "9fbf120d-6301-42d9-8c58-25e699a21dbd"
+controlPointID = "69d1d8f3-45e1-49a8-9821-9bbdfdaad9d9"
+dataSourceID = "22eac6e9-24d6-4bb5-be44-b36ace7c7bfb"
 
 bus = dbus.SystemBus()
 hci = bus.get_object("org.bluez", "/org/bluez/%s" % hciID)
@@ -52,7 +59,7 @@ for deviceID in devices:
         service = bus.get_object("org.bluez", "/org/bluez/%s/%s/%s" % (hciID, deviceID, serviceID))
         props = dbus.Interface(service, "org.freedesktop.DBus.Properties")
         id = props.Get("org.bluez.GattService1", "UUID")
-        if id != ancsId:
+        if id != ancsID:
             continue
         print("Found an iPhone!")
         characteristics = service.Introspect()
@@ -63,16 +70,20 @@ for deviceID in devices:
             characteristic = bus.get_object("org.bluez", "/org/bluez/%s/%s/%s/%s" % (hciID, deviceID, serviceID, characteristicID))
             props = dbus.Interface(characteristic, "org.freedesktop.DBus.Properties")
             id = props.Get("org.bluez.GattCharacteristic1", "UUID")
-            if id == notificationSourceId:
+            if id == notificationSourceID:
                 notificationSource = characteristic
-            elif id == controlPointId:
+                iphoneID = deviceID
+            elif id == controlPointID:
                 controlPoint = characteristic
-            elif id == dataSourceId:
+            elif id == dataSourceID:
                 dataSource = characteristic
 
 if notificationSource is None or controlPoint is None or dataSource is None:
     print("iPhone ANCS not found!")
     exit(1)
+
+device = bus.get_object("org.bluez", "/org/bluez/%s/%s" % (hciID, iphoneID))
+battery = dbus.Interface(device, "org.freedesktop.DBus.Properties")
 
 nsControl = dbus.Interface(notificationSource, "org.bluez.GattCharacteristic1")
 nsProps = dbus.Interface(notificationSource, "org.freedesktop.DBus.Properties")
@@ -85,8 +96,10 @@ nsControl.StartNotify()
 dsControl.StartNotify()
 nsLastMsg = nsProps.Get("org.bluez.GattCharacteristic1", "Value")
 dsLastMsg = dsProps.Get("org.bluez.GattCharacteristic1", "Value")
+batteryLast = battery.Get("org.bluez.Battery1", "Percentage")
 while True:
     time.sleep(1.0 / resolution)
+
     msg = nsProps.Get("org.bluez.GattCharacteristic1", "Value")
     if msg != nsLastMsg:
         nsLastMsg = msg
@@ -100,25 +113,28 @@ while True:
             ))
             print("New notification! Asking for details...")
             cpControl.WriteValue(fetchAttributes, {})
+
     msg = dsProps.Get("org.bluez.GattCharacteristic1", "Value")
     if msg != dsLastMsg:
         dsLastMsg = msg
         print("Notification details received!")
         msg = bytearray(msg)
-        appIdSize, msg = struct.unpack("<BIBH", msg[:8])[3], msg[8:]
-        appId, msg = msg[:appIdSize], msg[appIdSize:]
+        appIDSize, msg = struct.unpack("<BIBH", msg[:8])[3], msg[8:]
+        appID, msg = msg[:appIDSize], msg[appIDSize:]
         titleSize, msg = struct.unpack("<BH", msg[:3])[1], msg[3:]
         title, msg = msg[:titleSize], msg[titleSize:]
         messageSize, msg = struct.unpack("<BH", msg[:3])[1], msg[3:]
         message, msg = msg[:messageSize], msg[messageSize:]
 
-        appId = appId.decode('utf8', errors='ignore')
+        appID = appID.decode('utf8', errors='ignore')
         title = title.decode('utf8', errors='ignore')
         message = message.decode('utf8', errors='ignore')
-        print("From: %s (%s)" % (title, appId))
+        print("From: %s (%s)" % (title, appID))
         print(message)
+        tryRunning(['handlers/notification.sh', title, appID, message])
 
-        try:
-            subprocess.Popen(['./onNotification.sh', title, appId, message])
-        except Exception as e:
-            print("Error: %s" % str(e))
+    batteryState = battery.Get("org.bluez.Battery1", "Percentage")
+    if batteryState != batteryLast:
+        batteryLast = batteryState
+        print("Battery is at %d percent" % batteryState)
+        tryRunning(['handlers/battery.sh', batteryState])
