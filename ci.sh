985b8d07681e41785fc589149fe520f8ba5d325c
Pawel Zmarzly <pzmarzly0@gmail.com>
2026-05-24 01:39:45 +0000

    migrate from poetry to uv

--- ci.sh
+++ ci.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -eu
 
-python3 -m venv .venv
-source .venv/bin/activate
-poetry check --lock
-poetry install
+uv sync
 
-ancs4linux-advertising --help
-ancs4linux-ctl --help
-ancs4linux-desktop-integration --help
-ancs4linux-observer --help
+uv run ancs4linux-advertising --help
+uv run ancs4linux-ctl --help
+uv run ancs4linux-desktop-integration --help
+uv run ancs4linux-observer --help
 
-black --check ancs4linux
-isort --check ancs4linux
-autoflake --check --remove-unused-variables --remove-all-unused-imports -r ancs4linux
-mypy --namespace-packages -p ancs4linux
+uv run black --check ancs4linux
+uv run isort --check ancs4linux
+uv run autoflake --check --remove-unused-variables --remove-all-unused-imports -r ancs4linux
+uv run mypy --namespace-packages -p ancs4linux
